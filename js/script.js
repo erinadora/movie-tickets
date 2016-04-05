2672d3356d8a4bd55f7842aabfecffa63c53c91e
Erin Goncer and Elissa Sosland <erin@>
2016-04-05 16:02:58 -0700

    styled

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// BUSINESS LOGIC //
+
+function Tickets(movie, price, time) {
+  this.movie = movie;
+  this.price = price;
+  this.time = time;
+}
+
+
+
+
+// UI LOGIC //
+
+
+$(".dropdown-menu li a").click(function(){
